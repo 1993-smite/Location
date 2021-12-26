b05e0f20c4c682d6397dc530ede19f6a19dea8c9
Alex Sasov <sasov1993@mail.ru>
2021-12-26 14:54:54 +0300

    добавил просмотрочщик редиса

---
+++ Redis/Program.cs
@@ -0,0 +1,25 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Linq;
+using System.Net;
+
+namespace Redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnectionMultiplexer connection = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = connection.GetDatabase();
+            EndPoint endPoint = connection.GetEndPoints().First();
+            IServer server = connection.GetServer(endPoint);
+
+            foreach (var key in server.Keys(pattern: "**"))
+            {
+                Console.WriteLine($"key: {key}, value: {db.StringGet(key)}");
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
